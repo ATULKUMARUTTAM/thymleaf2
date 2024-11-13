0d3a9184b5bead17fb3875cfab852a1317d26945
ATULKUMARUTTAM <atulkumaruttam@gmail.com>
2024-11-13 23:07:29 +0530

    Initial commit

---
+++ src/main/java/com/atuluttam/thymleaf2/Thymleaf2Application.java
@@ -0,0 +1,13 @@
+package com.atuluttam.thymleaf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymleaf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymleaf2Application.class, args);
+	}
+
+}
